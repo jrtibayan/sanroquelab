659aaa5adf916b09b3af2eb155b6b1dfc102ce57
Jeric <jrtibayan@gmail.com>
2023-11-12 13:26:06 +0800

    chore: updated code with secret key

--- routes/patients.js
+++ routes/patients.js
@@ -243,7 +243,7 @@ router.post(
                   return JSON.parse(JSON.stringify(src))
                 }
 
-                const token = jwt.sign(convertToPlainObj(user), conf.secret)
+                const token = jwt.sign(convertToPlainObj(user), process.env.SECRET_KEY)
 
                 return res.json({
                   success: true,
