cc1056427b455ac4314b4428f52391f9f61b93c2
Jeric <jrtibayan@gmail.com>
2023-11-16 13:33:03 +0800

    chore: added shred dlog function for frontend

--- angular-src/src/app/shared/utilities.functions.ts
+++ angular-src/src/app/shared/utilities.functions.ts
@@ -7,4 +7,24 @@ export class Utilities {
         const formattedDateTime = `${givenDate.toLocaleDateString()}`;
         return formattedDateTime;
     }
+
+    static dlog(msg: any, logType: 'log' | 'warn' | 'error' | 'debug' = 'log'): void {
+        switch (logType) {
+          case 'log':
+            console.log(msg);
+            break;
+          case 'warn':
+            console.warn(msg);
+            break;
+          case 'error':
+            console.error(msg);
+            break;
+          case 'debug':
+            // Note: console.debug might not be available in all browsers
+            console.debug(msg);
+            break;
+          default:
+            console.log(msg);
+        }
+      }
 }
