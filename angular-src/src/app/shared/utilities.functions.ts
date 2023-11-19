bc64000b7345471c2329d5bfe5e4cfecaae46306
Jeric <jrtibayan@gmail.com>
2023-11-19 19:49:45 +0800

    feat: added utility function for formating date to be ready for date input tag

--- angular-src/src/app/shared/utilities.functions.ts
+++ angular-src/src/app/shared/utilities.functions.ts
@@ -8,6 +8,14 @@ export class Utilities {
         return formattedDateTime;
     }
 
+    static formatDateForInput(date: Date): string {
+      // Helper function to format a date as 'YYYY-MM-DD'
+      const year = date.getFullYear();
+      const month = ('0' + (date.getMonth() + 1)).slice(-2); // Month is zero-based
+      const day = ('0' + date.getDate()).slice(-2);
+      return `${year}-${month}-${day}`;
+    }
+
     static dlog(msg: any, logType: 'log' | 'warn' | 'error' | 'debug' = 'log'): void {
         switch (logType) {
           case 'log':
