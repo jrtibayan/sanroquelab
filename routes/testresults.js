d8df33606ab62cfa8266965076714fc66bf2ad54
Jeric <jrtibayan@gmail.com>
2023-12-21 06:42:47 +0800

    feat: route for updating urinalysis result and getting result by date is now available. and some minor cleanup in adding urinalysis result

--- routes/testresults.js
+++ routes/testresults.js
@@ -37,6 +37,42 @@ router.get(
     }
 )
 
+// Returns an array containing all transactions
+router.get(
+    '/getbydate',
+    passport.authenticate('jwt', { session: false }),
+    (req, res, next) => {
+      const startDate = req.query.startDate || '1900-01-01';
+      const endDate = req.query.endDate || '2300-12-31';
+      const status = req.query.status;
+
+      TestResult.getTestResultByDate(
+            startDate,
+            endDate,
+            status,
+            (err, testResult) => {
+                if (err) {
+                    return res.json({ success: false, msg: 'Failed to get test results' });
+                }
+  
+                if (testResult.length > 0) {
+                    return res.json({
+                        success: true,
+                        msg: 'Successfuly retrieved test results!',
+                        testResults: testResult
+                    });
+                } else {
+                    return res.json({
+                      success: false,
+                      msg: 'No test results found within the selected dates',
+                      testResult: []
+                    });
+                }
+            }
+        )
+    }
+  )
+
  // Register
 router.post(
     '/register',
@@ -109,7 +145,7 @@ router.post(
 );
 
 
- // Register
+// Save Urinalysis Result
 router.post(
     '/urinalysis/register',
     passport.authenticate('jwt', { session: false }),
@@ -142,21 +178,6 @@ router.post(
                     });
                 } else {
                     h.dlog('Result registered');
-
-                    /*
-                    let pendingTestId = null;
-                    for(const test of req.body.testsAndResults) {
-                        pendingTestId = test._id;
-                        h.dlog("trying to delete record with id "+ pendingTestId);
-                        PendingTest.deleteById(pendingTestId, (err) => {
-                            if (err) {
-                                h.dlog('Error deleting document: ' + err, 'error');
-                            } else {
-                                h.dlog('Document deleted successfully.');
-                            }
-                        });
-                    }*/
-
                     return res.json(h.appRes(
                         { success: true, msg: 'Result added' },
                         { id: result._id, patientName: result.patientName }
@@ -172,6 +193,52 @@ router.post(
         }
     }
 );
+
+
+// Update TestResult by _id
+router.put(
+    '/urinalysis/update/:id',
+    passport.authenticate('jwt', { session: false }), async (req, res) => {
+    const id = req.params.id;
+
+    const action = 'Update Result';
+
+    if (req.user && req.user.role && (req.user.role === "admin" || req.user.allowedActions && req.user.allowedActions.includes(action))) {
+        try {
+            // Find the TestResult by _id
+            const testResult = await TestResult.findById(id);
+
+            if (!testResult) {
+                return res.status(404).json({ success: false, msg: 'TestResult not found' });
+            }
+            
+            // Update only the fields provided in the request body
+            for (const key in req.body) {
+                if (key in testResult) {
+                    testResult[key] = req.body[key];
+                }
+            }
+
+            // Save the updated TestResult
+            const updatedTestResult = await testResult.save();
+
+            h.dlog('Result updated');
+            return res.json(h.appRes(
+                { success: true, msg: 'Result updated' },
+                { id: updatedTestResult._id, patientName: updatedTestResult.patientName }
+            ));
+        } catch (error) {
+            h.dlog(error, 'error');
+            return res.status(500).json({ success: false, msg: 'Internal Server Error' });
+        }
+    } else {
+        h.dlog('User not allowed to ' + action);
+        return res.status(401).json({
+            success: false,
+            msg: 'Unauthorized Access: User not allowed to ' + action
+        });
+    }
+});
 
 
 module.exports = router;
