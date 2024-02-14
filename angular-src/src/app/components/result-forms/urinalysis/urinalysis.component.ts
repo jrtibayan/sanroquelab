635a5ba523b28bf6695486efafdf18325a4fc99b
Jeric <jrtibayan@gmail.com>
2024-02-14 19:32:30 +0800

    chore: disable some inputs and hides params that are not paid on urinalysis form

--- angular-src/src/app/components/result-forms/urinalysis/urinalysis.component.ts
+++ angular-src/src/app/components/result-forms/urinalysis/urinalysis.component.ts
@@ -22,6 +22,7 @@ export class UrinalysisComponent {
   address!: String;
   gender!: String;
   age!: String;
+  requiredParameters: any;
 
   color!: String;
   transparency!: String;
@@ -79,6 +80,7 @@ export class UrinalysisComponent {
   ngOnInit(): void { 
     // Retrieve the ptest ID from the route parameters
     this.transactionData = this.sharedService.getSharedData();
+    this.requiredParameters = this.transactionData.test.parameters;
 
     this.authService.getProfile().subscribe(res => {
       let profile = {} as any;
@@ -138,7 +140,11 @@ export class UrinalysisComponent {
       return false
     });
   }
-  
+
+  // Check if there is an object with property name "Sugar" in this.requiredParameters
+  hasParameter(paramName): boolean {
+    return this.requiredParameters.some(param => param.name.toLowerCase() === paramName.toLowerCase());
+  }
 
   onSaveSubmit(){ 
     this.transactionData.test.parameters = [];
